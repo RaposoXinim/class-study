e516797e770af349cc77e6561b6f4adcd17f5aa9
Caio <82059034+RaposoXinim@users.noreply.github.com>
2024-04-17 20:01:06 -0300

    Add files via upload

--- banco.py
+++ banco.py
@@ -15,48 +15,48 @@ class BancoDeDados:
         except sqlite3.Error as e:
             print(f"Erro ao conectar ao banco de dados: {e}")
 
-def criar_tabelas(self):
-    self.criar_tabela_pessoa()
-    self.criar_tabela_marca()
-    self.criar_tabela_veiculo()
+    def criar_tabelas(self):
+        self.criar_tabela_pessoa()
+        self.criar_tabela_marca()
+        self.criar_tabela_veiculo()
 
-def criar_tabela_pessoa(self):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                """CREATE TABLE IF NOT EXISTS Pessoa (
-                        cpf INTEGER PRIMARY KEY,
-                        nome TEXT NOT NULL,
-                        nascimento DATE,
-                        oculos BOOLEAN
-                        )"""
-            )
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao criar tabela Pessoa: {e}")
+    def criar_tabela_pessoa(self):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    """CREATE TABLE IF NOT EXISTS Pessoa (
+                            cpf INTEGER PRIMARY KEY,
+                            nome TEXT NOT NULL,
+                            nascimento DATE,
+                            oculos BOOLEAN
+                            )"""
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao criar tabela Pessoa: {e}")
 
-def criar_tabela_marca(self):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                """CREATE TABLE IF NOT EXISTS Marca (
-                        id INTEGER PRIMARY KEY,
-                        nome TEXT NOT NULL,
-                        sigla TEXT
-                        )"""
-            )
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao criar tabela Marca: {e}")
+    def criar_tabela_marca(self):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    """CREATE TABLE IF NOT EXISTS Marca (
+                            id INTEGER PRIMARY KEY,
+                            nome TEXT NOT NULL,
+                            sigla TEXT
+                            )"""
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao criar tabela Marca: {e}")
 
-def criar_tabela_veiculo(self):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                """CREATE TABLE IF NOT EXISTS Veiculo (
+    def criar_tabela_veiculo(self):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    """CREATE TABLE IF NOT EXISTS Veiculo (
                         placa TEXT PRIMARY KEY,
                         cor TEXT NOT NULL,
                         cpf_proprietario INTEGER,
@@ -64,104 +64,139 @@ def criar_tabela_veiculo(self):
                         FOREIGN KEY(cpf_proprietario) references Pessoa(cpf),
                         FOREIGN KEY(id_marca) REFERENCES Marca(id)
                         )"""
-            )
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao criar tabela Veiculo: {e}")
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao criar tabela Veiculo: {e}")
 
-def inserir_pessoa(self, pessoa: Pessoa):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("INSERT INTO Pessoa VALUES (?, ?, ?, ?)", 
-            (pessoa.cpf, pessoa.nome, pessoa.nascimento, pessoa.oculos),)
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao inserir pessoa: {e}")
+    def inserir_pessoa(self, pessoa: Pessoa):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("INSERT INTO Pessoa VALUES (?, ?, ?, ?)", 
+                (pessoa.cpf, pessoa.nome, pessoa.nascimento, pessoa.oculos),)
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao inserir pessoa: {e}")
 
-def inserir_marca(self, marca: Marca):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("INSERT INTO Marca VALUES (?, ?, ?, ?)", 
-            (marca.id, marca.nome, marca.sigla),)
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao inserir marca: {e}")
+    def inserir_marca(self, marca: Marca):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("INSERT INTO Marca VALUES (?, ?, ?, ?)", 
+                (marca.id, marca.nome, marca.sigla),)
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao inserir marca: {e}")
 
-def inserir_veiculo(self, veiculo: Veiculo):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                "INSERT INTO Veiculo VALUES (?, ?, ?, ?)",
-                (
-                    veiculo.placa,
-                    veiculo.cor,
-                    veiculo.proprietario.cpf,
-                    veiculo.marca.id,
-                ),
-            )
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao inserir veículo: {e}")
-
-def atualizar_pessoa(self, pessoa):
-    
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                "UPDATE Pessoa SET nome=?, nascimento=?, oculos=? WHERE cpf=?",
-                (pessoa.nome, pessoa.nascimento, pessoa.oculos, pessoa.cpf),
-            )
-        except sqlite3.Error as e:
-            print(f"Erro ao  atualizar pessoa: {e}")
-
-def atualizar_veiculo(self, veiculo):
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                "UPDATE Veiculo SET cor=?, cpf_proprietario=?, id_marca=? where placa=?",
-                (   
-                    veiculo.cor,
-                    veiculo.proprietario.cpf,
-                    veiculo.marca.id,
-                    veiculo.placa,
+    def inserir_veiculo(self, veiculo: Veiculo):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    "INSERT INTO Veiculo VALUES (?, ?, ?, ?)",
+                    (
+                        veiculo.placa,
+                        veiculo.cor,
+                        veiculo.proprietario.cpf,
+                        veiculo.marca.id,
                     ),
-            )
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Falha ao atualizar o veiculo: {e}")
+                )
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao inserir veículo: {e}")
 
-def atualizar_marca(self, marca):
+    def atualizar_pessoa(self, pessoa):
     
-    if self.conn:
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(
-                "UPDATE Marca SET nome=?, sigla=? WHERE id=?",
-                (marca.nome, marca.sigla, marca.id),
-            )
-        except sqlite3.Error as e:
-            print(f"Erro ao  atualizar marca: {e}")
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    "UPDATE Pessoa SET nome=?, nascimento=?, oculos=? WHERE cpf=?",
+                    (pessoa.nome, pessoa.nascimento, pessoa.oculos, pessoa.cpf),
+                )
+            except sqlite3.Error as e:
+                print(f"Erro ao  atualizar pessoa: {e}")
 
-def apagar_veiculo(self, veiculo):
-    if self.conn:
-        try:
-            cursor= self.conn.cursor()
-            cursor.execute("DELETE FROM Veiculo WHERE placa=?", (veiculo.placa),)
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao apagar veiculo: {e}")
+    def atualizar_veiculo(self, veiculo):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    "UPDATE Veiculo SET cor=?, cpf_proprietario=?, id_marca=? where placa=?",
+                        (   
+                        veiculo.cor,
+                        veiculo.proprietario.cpf,
+                        veiculo.marca.id,
+                        veiculo.placa,
+                        ),
+                )   
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Falha ao atualizar o veiculo: {e}")
 
-def apagar_pessoa(self, pessoa):
-    if self.conn:
-        try:
-            cursor= self.conn.cursor()
-            cursor.execute("DELETE FROM Pessoa WHERE cpf=?", (pessoa.cpf),)
-            self.conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erro ao apagar veiculo: {e}")
+    def atualizar_marca(self, marca):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(
+                    "UPDATE Marca SET nome=?, sigla=? WHERE id=?",
+                    (marca.nome, marca.sigla, marca.id),
+                )
+            except sqlite3.Error as e:
+                print(f"Erro ao  atualizar marca: {e}")
+
+    def apagar_veiculo(self, veiculo):
+        if self.conn:
+            try:
+                cursor= self.conn.cursor()
+                cursor.execute("DELETE FROM Veiculo WHERE placa=?", (veiculo.placa),)
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao apagar veiculo: {e}")
+
+    def apagar_pessoa(self, pessoa):
+        if self.conn:
+            try:
+                cursor= self.conn.cursor()
+                cursor.execute("DELETE FROM Pessoa WHERE cpf=?", (pessoa.cpf),)
+                self.conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erro ao apagar veiculo: {e}")
+    def buscar_todas_pessoas(self):
+        pessoas=[]
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Pessoa")
+                for row in cursor.fetchall():
+                    cpf, nome, nascimento, oculos = row
+                    pessoas.append(Pessoa(cpf, nome, nascimento, oculos))
+            except sqlite3.Error as e: print(f"Erro ao buscar pessoa: {e}")
+        return pessoas
+    def buscar_todos_veiculos(self):
+        veiculos = []
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Veiculo")
+                for row in cursor.fetchall():
+                    placa, cor, cpf_proprietario, id_marca = row
+                    proprietario =self.buscar_pessoa_por_cpf(cpf_proprietario)
+                    marca= self.buscar_marca_por_id(id_marca)
+                    veiculos.append(Veiculo(placa, cor, proprietario, marca))
+            except sqlite3.Error as e: print(f"Erro ao buscar veiculo: {e}")
+        return veiculos
+    def buscar_todas_marcas(self):
+        marcas=[]
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("SELECT * FROM Marcas")
+                for row in cursor.fetchall():
+                    id, nome, sigla = row
+                    marcas.append(Marca(id, nome, sigla))
+            except sqlite3.Error as e: print(f"Erro ao buscar marcas: {e}")
+        return marcas
+
 
---
+++ main.py
@@ -0,0 +1,30 @@
+from banco import BancoDeDados
+from pessoa import Pessoa
+from marca import Marca
+from veiculo import Veiculo
+
+if __name__ == "__main__":
+    banco = BancoDeDados()
+    banco.conectar()
+    banco.criar_tabelas()
+    pessoa1 = Pessoa(cpf=12345678900, nome="Raphael", nascimento="1984-07-26", oculos=True)
+    banco.inserir_pessoa(pessoa1)
+    
+    marca1= Marca(id=1, nome="FIAT", sigla="FIA")
+    banco.inserir_marca(marca1)
+    
+    veiculo1=Veiculo(placa="LREW1I27", cor="Cinza", proprietario=pessoa1, marca=marca1)
+    banco.inserir_veiculo(veiculo1)
+
+    print("\nPessoas:")
+    for pessoa in banco.buscar_todas_pessoas():
+        print(pessoa)
+''' 
+    print("\nMarcas:)
+    for marca in banco.buscar_todas_marcas():
+        print(marca)
+    print("\nVeículos:")
+    for veiculo in banco.buscar_todos_veiculos():
+        print(veiculo)
+'''  
+#banco.fechar_conexao()
