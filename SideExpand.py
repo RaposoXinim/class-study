e11baf6469521035f9f06a6a9147caeb56c9b9d8
Caio <82059034+RaposoXinim@users.noreply.github.com>
2024-05-15 19:24:04 -0300

    Add files via upload

---
+++ SideExpand.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+janela = tk.Tk()
+frame1= tk.Frame(master=janela,height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+frame2= tk.Frame(master=janela,height=50, bg="green")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+frame3= tk.Frame(master=janela,height=25, bg="blue")
+frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+janela.mainloop()
+
